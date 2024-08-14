c559647a54ee6c3f3f97b4268cf971d59b37f001
Matheus Ferrari <matheusap.ferrari@hotmail.com>
2024-08-14 08:58:03 -0300

    .

--- prisma/migrations/20240814115119_add_gender_enum/migration.sql
+++ prisma/migrations/20240814115119_add_gender_enum/migration.sql
@@ -7,14 +7,20 @@ CREATE TYPE "Status" AS ENUM ('PAYMENT_PENDING', 'PAYMENT_APPROVED', 'PAYMENT_RE
 -- CreateEnum
 CREATE TYPE "PaymentMethod" AS ENUM ('CREDIT_CARD', 'DEBIT_CARD', 'PIX', 'BOLETO');
 
+-- CreateEnum
+CREATE TYPE "Genders" AS ENUM ('MASC', 'FEM', 'OTHER', 'NOTTOSAY');
+
 -- CreateTable
 CREATE TABLE "usuarios" (
     "id" TEXT NOT NULL,
     "nome" TEXT NOT NULL,
-    "cpf" BIGINT NOT NULL,
+    "cpf" TEXT NOT NULL,
     "email" TEXT NOT NULL,
     "senha" TEXT NOT NULL,
     "cargo" "Role" NOT NULL DEFAULT 'USER',
+    "celular" BIGINT,
+    "data-nascimento" TIMESTAMP(3) NOT NULL,
+    "genero" "Genders",
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT "usuarios_pkey" PRIMARY KEY ("id")
@@ -43,8 +49,10 @@ CREATE TABLE "carrinho-de-compras" (
 -- CreateTable
 CREATE TABLE "endereco-usuario" (
     "id" SERIAL NOT NULL,
+    "nome-destinatario" TEXT NOT NULL,
     "usuario_id" TEXT NOT NULL,
     "endereco" TEXT NOT NULL,
+    "tipo-endereco" TEXT NOT NULL,
     "numero" INTEGER NOT NULL,
     "bairro" TEXT NOT NULL,
     "cidade" TEXT NOT NULL,
@@ -67,22 +75,14 @@ CREATE TABLE "produtores" (
 );
 
 -- CreateTable
-CREATE TABLE "recriar-senha" (
-    "email" TEXT NOT NULL,
-    "token" TEXT NOT NULL,
-    "criado-em" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "nova-senha-usuario" TEXT NOT NULL,
-    "usuario_id" TEXT NOT NULL
-);
-
--- CreateTable
 CREATE TABLE "produtos" (
     "id" SERIAL NOT NULL,
     "descricao" TEXT NOT NULL,
     "titulo" TEXT NOT NULL,
     "preco" DECIMAL(65,30) NOT NULL,
-    "quantidade" BIGINT NOT NULL,
+    "quantidade" INTEGER NOT NULL,
     "palavras-chaves" TEXT NOT NULL,
+    "url-imagem" TEXT NOT NULL,
     "criado-em" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "produtor_id" TEXT NOT NULL,
 
@@ -90,22 +90,9 @@ CREATE TABLE "produtos" (
 );
 
 -- CreateTable
-CREATE TABLE "comentario-produtos" (
-    "id" SERIAL NOT NULL,
-    "titulo" TEXT NOT NULL,
-    "descricao" TEXT NOT NULL,
-    "numero-avaliacoes" INTEGER NOT NULL,
-    "produto_id" INTEGER NOT NULL,
-    "criado-em" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-
-    CONSTRAINT "comentario-produtos_pkey" PRIMARY KEY ("id")
-);
-
--- CreateTable
 CREATE TABLE "vendas" (
     "id" SERIAL NOT NULL,
     "usuario_id" TEXT NOT NULL,
-    "produtos-id" INTEGER[],
     "endereco-cliente-id" INTEGER NOT NULL,
     "total" DECIMAL(65,30) NOT NULL,
     "status-pedido" "Status" NOT NULL DEFAULT 'PAYMENT_PENDING',
@@ -113,6 +100,16 @@ CREATE TABLE "vendas" (
     "vendido-em" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT "vendas_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "produtos-venda" (
+    "id" SERIAL NOT NULL,
+    "produto_id" INTEGER NOT NULL,
+    "venda_id" INTEGER,
+    "quantidade" BIGINT NOT NULL,
+
+    CONSTRAINT "produtos-venda_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
@@ -126,12 +123,6 @@ CREATE UNIQUE INDEX "produtores_userId_key" ON "produtores"("userId");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "produtores_cnpj_key" ON "produtores"("cnpj");
-
--- CreateIndex
-CREATE UNIQUE INDEX "recriar-senha_email_key" ON "recriar-senha"("email");
-
--- CreateIndex
-CREATE INDEX "recriar-senha_email_token_idx" ON "recriar-senha"("email", "token");
 
 -- AddForeignKey
 ALTER TABLE "produtos-curtidos" ADD CONSTRAINT "produtos-curtidos_produto_id_fkey" FOREIGN KEY ("produto_id") REFERENCES "produtos"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
@@ -152,13 +143,13 @@ ALTER TABLE "endereco-usuario" ADD CONSTRAINT "endereco-usuario_usuario_id_fkey"
 ALTER TABLE "produtores" ADD CONSTRAINT "produtores_userId_fkey" FOREIGN KEY ("userId") REFERENCES "usuarios"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "recriar-senha" ADD CONSTRAINT "recriar-senha_usuario_id_fkey" FOREIGN KEY ("usuario_id") REFERENCES "usuarios"("id") ON DELETE CASCADE ON UPDATE CASCADE;
-
--- AddForeignKey
 ALTER TABLE "produtos" ADD CONSTRAINT "produtos_produtor_id_fkey" FOREIGN KEY ("produtor_id") REFERENCES "produtores"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "comentario-produtos" ADD CONSTRAINT "comentario-produtos_produto_id_fkey" FOREIGN KEY ("produto_id") REFERENCES "produtos"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "vendas" ADD CONSTRAINT "vendas_usuario_id_fkey" FOREIGN KEY ("usuario_id") REFERENCES "usuarios"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "vendas" ADD CONSTRAINT "vendas_usuario_id_fkey" FOREIGN KEY ("usuario_id") REFERENCES "usuarios"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "produtos-venda" ADD CONSTRAINT "produtos-venda_venda_id_fkey" FOREIGN KEY ("venda_id") REFERENCES "vendas"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "produtos-venda" ADD CONSTRAINT "produtos-venda_produto_id_fkey" FOREIGN KEY ("produto_id") REFERENCES "produtos"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
